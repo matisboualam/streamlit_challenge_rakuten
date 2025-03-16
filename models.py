ff7386814da2c8c640b90f88c7b893d432782aa3
matisboualam <matis112000@hotmail.fr>
2025-03-16 18:28:19 +0100

    vfmlv

--- models.py
+++ models.py
@@ -17,13 +17,13 @@ access_key_id = st.secrets["dvc"]["access_id"]
 subprocess.run(['dvc', 'remote', 'list'])
 
 subprocess.run([
-    'dvc', 'remote', 'modify', 'origin', 
-    f'access_key_id={access_key_id}'
+    'dvc', 'remote', 'modify', 'origin', '--local', 
+    'access_key_id', f'{access_key_id}'
 ])
 
 subprocess.run([
-    'dvc', 'remote', 'modify', 'origin', 
-    f'secret_access_key={access_key_id}'
+    'dvc', 'remote', 'modify', 'origin', '--local', 
+    'secret_access_key', f'{access_key_id}'
 ])
 
 subprocess.run(['dvc', 'pull'])
